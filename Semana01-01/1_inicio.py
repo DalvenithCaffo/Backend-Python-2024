74b22d918b51ed94fb826b6e5eb00f5961bb131c
Dalvenith <dcffa86@gmail.com>
2024-04-29 13:47:39 -0500

    Inicios en Python - variables y listas

---
+++ Semana01-01/1_inicio.py
@@ -0,0 +1,70 @@
+
+nombre = 'Eduardo'
+nombre = 12 
+nombre = False 
+nombre = '2023-12-12'
+
+# Creo la variable par aindicar el estado civil de la persona.
+soltero = False
+
+# El comentario es importante para mantener nuestro código documentado.
+print(nombre)
+
+numero1 = 10 
+numero2 = 50
+
+print(type(numero2))
+
+resultado = numero1 + numero2 
+
+print(resultado)
+
+
+numero1 = '10'
+numero2 = "50"
+
+resultado = numero1 + numero2 
+
+print(resultado)
+
+#para convertir de un tipo de dato a otro uso el tipo de dato que quiero convertir
+convertir_entero = int(numero1)
+print(type(convertir_entero))
+
+# TIPOS DE VARIABLES
+
+## INT (entero)
+numero = 10 
+
+## FLOAT (flotantes)
+### Si ponemos coma esto se puede confundir con un tipo de dato especial (ARREGLO, TUPLA, DICIONARIO, SET)
+calificaciones = 10.4
+
+## BOOLEAN (Boolean)
+vacunado = False 
+
+## STRING (text) 
+
+nombre = 'eduardo "carnicero"'
+nombre = "eduard'o"
+texto = ''' 
+Era una mañana de enero. 
+Los pajaros cantaban ... 
+'''
+
+texto2 = """ 
+Y en eso, una señora 
+Muy amablemente me sonrio y me dijo "...."
+"""
+
+### Si queremos que el texto contengan saltos de linea usamos triple comilla o triple doble comilla 
+
+curso, grupo, mes, dia, hora, minutos = 'Backend', 'Clase01', 'Mayo' , 1, 5, 11 
+
+### Formato para declarar varibles, funciones, clases, metodos
+snake_case = 'snake_case'
+nombre_completo = 'Dalvenith Caffo Abanto'
+
+camelCase = 'camelCase'
+
+PascalCase = 'PascalCase'
---
+++ Semana01-01/2_colecciones_de_datos.py
@@ -0,0 +1,16 @@
+#+ Puedo agrupar varios valores en una variable 
+
+### LISTAS
+# Que se puede modificar, es ordenada (maneja indices)
+
+alumnos = ['Dalvenith Aurelio Caffo', 'Diana Elizabeth Olarte', 'Diana Valentina Caffo', 'Diana Elizabeth Caffo']
+
+# Las listas empiezan con la posicion 0 
+print(alumnos[0])
+print(alumnos[3])
+
+# Para saber el contenido (longitud) de datos
+print(len(alumnos))
+
+# Para saber el contenido (longitud) de datos
+print(alumnos[-2])
